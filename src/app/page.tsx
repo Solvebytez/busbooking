6caad3e8cb1b3eeb54a2b4143b858624fbe48f60
Solvebytez <githubsolvebytez@gmail.com>
2024-11-28 21:28:40 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import PopularRoutes from "@/components/Home/PopularRoutes";
 import TourPackages from "@/components/Home/tourPackages";
 
 import SliderBanner from "@/components/Layout/Slider";
+import { Suspense } from "react";
 
 
 export default function Home() {
   return (
     <div className="">
      <div className="relative">
-        <SliderBanner/>
+       <Suspense fallback={"Loading..."}>
+       <SliderBanner/>
+       </Suspense>
        <div className="absolute bottom-[3%] px-[0%] md:px-[0%] xl:px-[4%] 2xl:px-[7%] w-full">
        <TicketBookingForm/>
        </div>
